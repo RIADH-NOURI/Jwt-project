19d7873004fb848edd781a92234360116ffbc2e6
RIADH <riadhnouri0502@gmail.com>
2024-11-05 21:52:25 +0100

    update files

--- client/src/api/authApi.jsx
+++ client/src/api/authApi.jsx
@@ -6,7 +6,6 @@ const api = axios.create({
     baseURL: BASE_URL,
 });
 
-// إضافة التوكن إلى طلبات HTTP
 api.interceptors.request.use(
     (config) => {
         const accessToken = localStorage.getItem('accessToken');
@@ -18,29 +17,23 @@ api.interceptors.request.use(
     (error) => Promise.reject(error)
 );
 
-// التعامل مع استجابة HTTP
 api.interceptors.response.use(
     (response) => response,
     async (error) => {
         const originalRequest = error.config;
 
-        // التعامل مع الأخطاء التي تشير إلى انتهاء صلاحية الـ accessToken
         if (error.response && error.response.status === 403 && !originalRequest._retry) {
             originalRequest._retry = true;
             try {
-                // استرجاع الـ refreshToken من localStorage
                 const refreshToken = localStorage.getItem('refreshToken');
 
-                // طلب تجديد الـ accessToken
                 const { data } = await axios.post(`${BASE_URL}/refresh`, { token: refreshToken });
 
-                // تخزين الـ accessToken و refreshToken الجديدين
                 localStorage.setItem('accessToken', data.accessToken);
                 if (data.refreshToken) {
-                    localStorage.setItem('refreshToken', data.refreshToken); // تخزين الـ refreshToken الجديد إذا كان موجودًا
+                    localStorage.setItem('refreshToken', data.refreshToken); 
                 }
 
-                // تحديث الـ Authorization header
                 originalRequest.headers.Authorization = `Bearer ${data.accessToken}`;
                 return api(originalRequest);
             } catch (refreshError) {
